77d343cea4804cc4267627d43a5ff20bb8909ffb
biku1998 <kumarsourabh228@gmail.com>
2020-11-03 22:46:26 +0530

    bug-fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/food-delivery-app/">
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/restaurants" exact component={Restaurants} />
